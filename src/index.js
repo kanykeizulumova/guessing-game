00e129481407886394005ebe7bcdb1b296ca36f3
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-05-04 14:06:31 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = 0;
+    this.max = 0;
+    this.number = 0;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.number = Math.ceil((this.min + this.max) / 2);
+    return this.number;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.number;
+  }
+
+  greater() {
+    this.min = this.number;
+  }
 }
 
 module.exports = GuessingGame;
